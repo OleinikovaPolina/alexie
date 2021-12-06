3484d031be62d9a90ec8973e51b142ef7d49d163
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-06 19:43:32 +0300

    video

--- js/copyhomescript.js
+++ js/copyhomescript.js
@@ -1,14 +1,16 @@
-window.addEventListener('load', () => {
-    document.querySelectorAll(".lll").forEach(el => {
-        el.addEventListener('click', () => {
-            console.log('f')
+"use strict";
+
+window.addEventListener('load', function () {
+    let t = document.querySelectorAll(".lll");
+    for (let i = 0; i < t.length; i++) {
+        t[i].addEventListener('click', function () {
             document.querySelector('.vk-video').classList.add('d-block');
             document.querySelector('.close-img').classList.add('d-block');
-        })
-    })
-    document.querySelector('.close-img').addEventListener('click', (event) => {
-        document.querySelector('.close-img').classList.remove('d-block')
+        });
+    }
+    document.querySelector('.close-img').addEventListener('click', function (event) {
+        document.querySelector('.close-img').classList.remove('d-block');
         document.querySelector('.vk-video').classList.remove('d-block');
         document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-135841332&id=456239063&hash=9e0840aa6d331cdd&hd=4&autoplay=1';
-    })
-})
+    });
+});

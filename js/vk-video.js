8a929570584b2320ea94fe736490742bef68b802
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-06 19:37:37 +0300

    video

--- js/vk-video.js
+++ js/vk-video.js
@@ -1,19 +1,21 @@
-window.addEventListener('load', () => {
-    document.querySelector(".vk-video-wrap").addEventListener('click', () => {
-        document.querySelector('.vk-video').classList.add('d-block');
-        document.querySelector('.close-img').classList.add('d-block');
-        document.querySelector('.btn-for-menu').classList.add('d-none');
-        document.querySelectorAll('.menu').forEach(el => {
-            el.style.zIndex = "0";
-        });
-    })
-    document.querySelector('.close-img').addEventListener('click', (event) => {
-        event.currentTarget.classList.remove('d-block')
-        document.querySelector('.vk-video').classList.remove('d-block');
-        document.querySelector('.vk-video').src='https://vk.com/video_ext.php?oid=-135841332&id=456239063&hash=9e0840aa6d331cdd&hd=4&autoplay=1';
-        document.querySelector('.btn-for-menu').classList.remove('d-none');
-        document.querySelectorAll('.menu').forEach(el => {
-            el.style.zIndex = "4";
-        });
-    })
-})
+"use strict";
+
+window.addEventListener('load', function () {
+  document.querySelector(".vk-video-wrap").addEventListener('click', function () {
+    document.querySelector('.vk-video').classList.add('d-block');
+    document.querySelector('.close-img').classList.add('d-block');
+    document.querySelector('.btn-for-menu').classList.add('d-none');
+    document.querySelectorAll('.menu').forEach(function (el) {
+      el.style.zIndex = "0";
+    });
+  });
+  document.querySelector('.close-img').addEventListener('click', function (event) {
+    event.currentTarget.classList.remove('d-block');
+    document.querySelector('.vk-video').classList.remove('d-block');
+    document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-13695781&id=456239507&hash=bc33811e79c66d35';
+    document.querySelector('.btn-for-menu').classList.remove('d-none');
+    document.querySelectorAll('.menu').forEach(function (el) {
+      el.style.zIndex = "4";
+    });
+  });
+});

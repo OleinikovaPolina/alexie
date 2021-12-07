f8c0360768415090f6de3d389bf8d4d84a887c36
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-07 19:52:36 +0300

    vr

--- js/copyhomescript.js
+++ js/copyhomescript.js
@@ -4,14 +4,27 @@ window.addEventListener('load', function () {
     let t = document.querySelectorAll(".lll");
     for (let i = 0; i < t.length; i++) {
         t[i].addEventListener('click', function () {
-            document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
-            document.querySelector('.vk-video').classList.add('d-block');
+            if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+                //ie
+                document.querySelector('.vk-videoIE').src = 'video/АлександрНевскийVR.mp4';
+                document.querySelector('.vk-videoIE').classList.add('d-block');
+                document.querySelector('.vk-videoIE').classList.play();
+            } else {
+                document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
+                document.querySelector('.vk-video').classList.add('d-block');
+            }
             document.querySelector('.close-img').classList.add('d-block');
         });
     }
     document.querySelector('.close-img').addEventListener('click', function (event) {
+        if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+            document.querySelector('.vk-videoIE').pause()
+            document.querySelector('.vk-videoIE').classList.remove('d-block');
+        } else {
+            document.querySelector('.vk-video').classList.remove('d-block');
+            document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&';
+        }
         document.querySelector('.close-img').classList.remove('d-block');
-        document.querySelector('.vk-video').classList.remove('d-block');
-        document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&';
+
     });
 });
--- js/vk-video.js
+++ js/vk-video.js
@@ -1,29 +1,40 @@
 "use strict";
 
 window.addEventListener('load', function () {
-
-  document.querySelector(".vk-video-wrap").addEventListener('click', function () {
-    document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
-
-    document.querySelector('.vk-video').classList.add('d-block');
+    document.querySelector(".vk-video-wrap").addEventListener('click', function () {
+      if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+        //ie
+        document.querySelector('.vk-videoIE').src = 'video/АлександрНевскийVR.mp4';
+        document.querySelector('.vk-videoIE').classList.add('d-block');
+        document.querySelector('.vk-videoIE').classList.play();
+      } else {
+        document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
+        document.querySelector('.vk-video').classList.add('d-block');
+      }
     document.querySelector('.close-img').classList.add('d-block');
     document.querySelector('.btn-for-menu').classList.add('d-none');
+    document.querySelector('.desc_home').classList.add('d-none');
     let t = document.querySelectorAll(".menu");
     for (let i = 0; i < t.length; i++) {
       t[i].style.zIndex = "0";
     }
-    // document.querySelectorAll('.menu').forEach(function (el) {
-    //   el.style.zIndex = "0";
-    // });
   });
-  document.querySelector('.close-img').addEventListener('click', function (event) {
+    document.querySelector('.close-img').addEventListener('click', function (event) {
     event.currentTarget.classList.remove('d-block');
-    document.querySelector('.vk-video').classList.remove('d-block');
-    document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&';
+      if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+        document.querySelector('.vk-videoIE').pause()
+        document.querySelector('.vk-videoIE').classList.remove('d-block');
+      } else {
+        document.querySelector('.vk-video').classList.remove('d-block');
+        document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&';
+      }
     document.querySelector('.btn-for-menu').classList.remove('d-none');
+    document.querySelector('.desc_home').classList.remove('d-none');
+
     let t = document.querySelectorAll(".menu");
     for (let i = 0; i < t.length; i++) {
       t[i].style.zIndex = "4";
     }
   });
+
 });

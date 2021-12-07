7cbf95c94572e2c5bc603bdef21ed571b76aae50
Ruslan <suleandiman2@mail.ru>
2021-12-07 16:34:18 +0300

    change link

--- js/copyhomescript.js
+++ js/copyhomescript.js
@@ -1,10 +1,10 @@
 "use strict";
 
 window.addEventListener('load', function () {
-    document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
     let t = document.querySelectorAll(".lll");
     for (let i = 0; i < t.length; i++) {
         t[i].addEventListener('click', function () {
+            document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-41185682&id=456239054&hash=a396983a15039fa1&hd=4&autoplay=1';
             document.querySelector('.vk-video').classList.add('d-block');
             document.querySelector('.close-img').classList.add('d-block');
         });

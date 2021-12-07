64e0ef6ce8774cf5ba9417521fb6aa63e46ebf49
Ruslan <suleandiman2@mail.ru>
2021-12-07 03:18:48 +0300

    vk video

--- js/vk-video.js
+++ js/vk-video.js
@@ -1,8 +1,9 @@
 "use strict";
 
 window.addEventListener('load', function () {
+  document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-135841332&id=456239063&hash=9e0840aa6d331cdd&hd=4&autoplay=1';
+
   document.querySelector(".vk-video-wrap").addEventListener('click', function () {
-    document.querySelector('.vk-video').src = 'https://vk.com/video_ext.php?oid=-135841332&id=456239063&hash=9e0840aa6d331cdd&hd=4&autoplay=1';
     document.querySelector('.vk-video').classList.add('d-block');
     document.querySelector('.close-img').classList.add('d-block');
     document.querySelector('.btn-for-menu').classList.add('d-none');

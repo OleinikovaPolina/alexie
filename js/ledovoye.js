6bffd33481fb8966efef2fcf6bb7549bd3d25406
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-07 18:29:41 +0300

    ledovoe

--- js/ledovoye.js
+++ js/ledovoye.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.cd-btn').addEventListener('click', (event) => {
         document.querySelector('.map').classList.remove('map_hover');
         event.currentTarget.classList.add('display_none');
-        
-        document.querySelectorAll('.cd-close').forEach((event) => {
-            event.addEventListener('click', (event) => {
+        let t = document.querySelectorAll('.cd-close');
+        for (let i = 0; i < t.length; i++) {
+            t[i].addEventListener('click', (event) => {
                 document.querySelector('.map').classList.add('map_hover');
                 document.querySelector('.cd-btn').classList.remove('display_none')
             })
-        })
+        }
+        // document.querySelectorAll('.cd-close').forEach((event) => {
+        //     event.addEventListener('click', (event) => {
+        //         document.querySelector('.map').classList.add('map_hover');
+        //         document.querySelector('.cd-btn').classList.remove('display_none')
+        //     })
+        // })
     })
 })

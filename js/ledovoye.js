eb8d260b0f14c0e77f6b4a1c1dc45d66c02f217c
OleinikovaPolina <90616548+OleinikovaPolina@users.noreply.github.com>
2021-12-07 21:21:53 +0300

    Update ledovoye.js

--- js/ledovoye.js
+++ js/ledovoye.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('.cd-btn').addEventListener('click', (event) => {
-        document.querySelector('.map').classList.remove('map_hover');
-        event.currentTarget.classList.add('display_none');
-        let t = document.querySelectorAll('.cd-close');
-        for (let i = 0; i < t.length; i++) {
-            t[i].addEventListener('click', (event) => {
-                document.querySelector('.map').classList.add('map_hover');
-                document.querySelector('.cd-btn').classList.remove('display_none')
-            })
-        }
-        // document.querySelectorAll('.cd-close').forEach((event) => {
-        //     event.addEventListener('click', (event) => {
-        //         document.querySelector('.map').classList.add('map_hover');
-        //         document.querySelector('.cd-btn').classList.remove('display_none')
-        //     })
-        // })
-    })
-})
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector('.cd-btn').addEventListener('click', function (event) {
+    document.querySelector('.map').classList.remove('map_hover');
+    event.currentTarget.classList.add('display_none');
+    var t = document.querySelectorAll('.cd-close');
+
+    for (var i = 0; i < t.length; i++) {
+      t[i].addEventListener('click', function (event) {
+        document.querySelector('.map').classList.add('map_hover');
+        document.querySelector('.cd-btn').classList.remove('display_none');
+      });
+    } // document.querySelectorAll('.cd-close').forEach((event) => {
+    //     event.addEventListener('click', (event) => {
+    //         document.querySelector('.map').classList.add('map_hover');
+    //         document.querySelector('.cd-btn').classList.remove('display_none')
+    //     })
+    // })
+
+  });
+});
